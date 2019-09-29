1aa3e1b3fe1ae81dee6eb5ef641ad5e4b1ad20df
Angie <angiec1223@gmail.com>
2019-09-28 17:29:40 -0700

    added files

---
+++ Library/Collab/Original/Assets/Scripts/Player_Score.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Player_Score : MonoBehaviour {
+    private float timeLeft = 5000;
+    public int playerscore = 0;
+    public bool level0;
+    public bool level1;
+    public bool levelBoos;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+/*	void Update () {
+        timeLeft -= Time.deltaTime;
+        if(timeLeft < 0.1f)
+        {
+            if(level0 == true) {
+                SceneManager.LoadScene("Charly Dang Level 0");
+            } else if (level1 == true)
+            {
+                SceneManager.LoadScene("Charly Dang Level 1");
+            } else if (levelBoss == true)
+            {
+                SceneManager.LoadScene("Charly Dang");
+            }
+            
+        }
+	}
+	*/
+}
